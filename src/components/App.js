f4b039c87a393a8a61438c9a51579b3ee839b9d2
AMutaa <piusandru@gmail.com>
2018-07-18 00:42:12 -0400

    new file update

---
+++ src/components/App.js
@@ -0,0 +1,150 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  static propTypes = {
+    match: PropTypes.object
+  };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    // first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  addFish = fish => {
+    //  Take a copy of the existing state
+    const fishes = {
+      ...this.state.fishes
+    };
+    // Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // Set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
+
+  updateFish = (key, updatedFish) => {
+    // Take a copy of the current state
+    const fishes = {
+      ...this.state.fishes
+    };
+    // Update that state
+    fishes[key] = updatedFish;
+    // Set that to state
+    this.setState({
+      fishes
+    });
+  };
+
+  deleteFish = key => {
+    // Take a copy of state
+    const fishes = {
+      ...this.state.fishes
+    };
+    // Update the state
+    fishes[key] = null;
+    // update state
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
+  };
+
+  addToOrder = key => {
+    // Take a copy of state
+    const order = {
+      ...this.state.order
+    };
+    // Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // Call setState tou update our state object
+    this.setState({
+      order
+    });
+  };
+
+  removeFromOrder = key => {
+    // Take a copy of state
+    const order = {
+      ...this.state.order
+    };
+    // remove that item form order
+    delete order[key];
+    // Call setState tou update our state object
+    this.setState({
+      order
+    });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Boston's Best" />
+          <ul className="fishes">
+            {" "}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}{" "}
+          </ul>{" "}
+        </div>{" "}
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
+        <Inventory
+          addFish={this.addFish}
+          updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ColorPicker extends React.Component {
+
+  render() =>
+    return (
+
+      <
+      h1 > This is a ColorPicker < /h1>
+    );
+
+
+}
